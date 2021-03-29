dbbab5b6ab74be84754891ece2c3284c0d3234d3
u-kai <76635578+u-kai@users.noreply.github.com>
2021-03-30 07:43:04 +0900

    ajust error a litle

--- application/applicant_app/views.py
+++ application/applicant_app/views.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,flash,redirect,url_for
 
 applicant_app = Blueprint("applicant_app", __name__, template_folder="templates", static_folder="./statics")
 
 @applicant_app.route("/")
 def index():
-    return render_template("applicant_app/index/html")
+    print("not dev yet")
+    flash("not dev yet")
+    return redirect(url_for("common_app.index"))
+    # return render_template("applicant_app/index/html")
+
+@applicant_app.route("/narrow_down")
+def narrow_down():
+    print("not dev yet")
+    flash("not dev yet")
+    return redirect(url_for("common_app.index"))
